4406e283e85785d44e97d1a5ff7ff7eaaef43743
Leah Nelson and Maarja Laineste <leah@>
2016-06-21 11:15:37 -0700

    add ability to assign user info to driver

--- js/scripts.js
+++ js/scripts.js
@@ -3,14 +3,14 @@
 
 //Ride constructor
 
-function Ride (from, to, date, time, seats, driver, price){
+function Ride (from, to, date, time, seats, price){
   this.id = null;
   this.from = from;
   this.to = to;
   this.date = date;
   this.time = time;
   this.seats = seats;
-  this.driver = driver;
+  this.driver = [];
   this.riders = [];
   this.price = price;
 }
@@ -20,6 +20,16 @@ function Ride (from, to, date, time, seats, driver, price){
 Ride.prototype.addRider = function(user) {
   this.riders.push(user);
   this.seats--;
+};
+
+Ride.prototype.addDriver = function(driverName, allUsersArray) {
+  var newDriverArray = [];
+  allUsersArray.forEach(function(user){
+    if (driverName === user.username) {
+      newDriverArray.push(user);
+    }
+  })
+  this.driver = newDriverArray;
 };
 
 //User constructor
@@ -35,18 +45,13 @@ function User (username, firstName, lastName, age, image){
 
 //User prototype methods
 
-User.prototype.methodName = function () {
-
-};
-
-
-function RideList () {
-  this.rides = [];
-};
-
-RideList.prototype.addRide = function (ride) {
-  this.rides.push(ride);
-};
+// function RideList () {
+//   this.rides = [];
+// };
+//
+// RideList.prototype.addRide = function (ride) {
+//   this.rides.push(ride);
+// };
 
 RideList.prototype.removeRide = function (rideId) {
   this.rides.splice(rideId, 1);
@@ -62,26 +67,10 @@ RideList.prototype.findRide = function (from) {
   return result;
 };
 
-
-
 // UI Logic
 $(document).ready(function() {
-  $("form#new-ride").submit(function(event) {
-    event.preventDefault();
-    var driver = $("#ride-driver").val();
-    var from = $("#ride-from").val();
-    var to = $("#ride-to").val();
-    var date = $("#ride-date").val();
-    var time = $("#ride-time").val();
-    var price = parseInt($("#ride-price").val());
-    var seats = parseInt($("#ride-seats").val());
-    var newRide = new Ride(from, to, date, time, seats, driver, price);
-    // newRide.addRider();
-    $("ul.ride-list").append("<li>" + newRide.from + " to " + newRide.to + "</li>");
-    console.log(newRide);
-    $("form").trigger("reset");
-  });
-
+  var allRides = [];
+  var allUsers = [];
   $("form#new-user").submit(function(event) {
     event.preventDefault();
     var username = $("#username").val();
@@ -90,8 +79,26 @@ $(document).ready(function() {
     var age = $("#age").val();
     var image = $("#image").val();
     var newUser = new User(username, firstName, lastName, age, image);
-    console.log(newUser);
+    allUsers.push(newUser);
     $("form").trigger("reset");
   });
 
+  $("form#new-ride").submit(function(event) {
+    event.preventDefault();
+    console.log(allUsers);
+    var drivername = $("#ride-driver").val();
+    console.log(drivername);
+    var locationFrom = $("#ride-from").val();
+    var to = $("#ride-to").val();
+    var date = $("#ride-date").val();
+    var time = $("#ride-time").val();
+    var price = parseInt($("#ride-price").val());
+    var seats = parseInt($("#ride-seats").val());
+    var newRide = new Ride(locationFrom, to, date, time, seats, price);
+    console.log(newRide.driver)
+    newRide.addDriver(drivername, allUsers);
+    allRides.push(newRide);
+    console.log(allRides);
+    $("form").trigger("reset");
+  });
 });
